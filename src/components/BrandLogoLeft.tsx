39403d73917a64df5812a0ee0475b74cfe361efd
wpinocho <julian.ruiz.loza@gmail.com>
2026-07-16 19:47:41 -0600

    AI: Updated 1 component(s)

--- src/components/BrandLogoLeft.tsx
+++ src/components/BrandLogoLeft.tsx
@@ -6,7 +6,7 @@ export const BrandLogoLeft = () => {
       <img
         src="https://ptgmltivisbtvmoxwnhd.supabase.co/storage/v1/object/public/message-images/b3071cbc-7f37-49dd-bb09-3f7c8ce155aa/1784252640328-k1wtkp1kd5.webp"
         alt="Smilo"
-        className="h-9 w-auto object-contain"
+        className="h-14 w-auto object-contain"
       />
     </Link>
   )
